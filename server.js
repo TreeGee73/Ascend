13c3fad02dd88d9eae5a267b318bcb5b6fc2bc2a
Alan Huynh <alan.hanhuynh@gmail.com>
2020-12-18 13:10:07 -0700

    api routes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("app/Develop"));
 
 require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+// require("./app/routes/html-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
