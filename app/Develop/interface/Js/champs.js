d8c736058bc1006bdb5ca7aff6992162efca5b8a
TreeGee73 <TreeGee73@gmail.com>
2020-12-18 14:37:02 -0700

    camps.js

---
+++ app/Develop/interface/Js/champs.js
@@ -0,0 +1,36 @@
+const player = [
+    {
+        classType='Warrior',
+        name='Bruth Spiritreaper',
+        health=200,
+        attack: 50,
+        defense: 40,
+        speed: 60
+    },
+    {
+        classType='Rogue',
+        name='Lacina',
+        health=120,
+        attack: 40,
+        defense: 20,
+        speed: 100
+    },
+    {
+        classType='Mage',
+        name='Ziu Qaim',
+        health=100,
+        attack: 150,
+        defense: 0,
+        speed: 75
+    },
+    {
+        classType='Hunter',
+        name='Gwendolyn Helton',
+        health=150,
+        attack: 90,
+        defense: 30,
+        speed: 80
+    }
+];
+
+module.exports = player;
