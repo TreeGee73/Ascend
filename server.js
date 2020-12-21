3fc7f92ad03e2df76dc1c689cc001d591f59c3d2
ajsnow <ajsnow56@gmail.com>
2020-12-21 13:01:18 -0700

    updating gamemanager

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-// const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
