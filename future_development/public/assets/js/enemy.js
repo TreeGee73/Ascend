2188688d8a9fb9a742a14d809e7d9d9cf0aa0da5
Theresa <TreeGee73@gmail.com>
2020-12-22 10:42:50 -0700

    organized

---
+++ future_development/public/assets/js/enemy.js
@@ -0,0 +1,11 @@
+let enemy;
+
+function Enemy(classType, health, attack, defense, speed, exp) {
+    this.classType = classType;
+    // this.name =name;
+    this.health = health;
+    this.attack = attack;
+    this.defense = defense;
+    this.speed = speed;
+    this.exp = exp;
+};
