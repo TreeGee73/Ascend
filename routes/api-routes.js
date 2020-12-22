3ebabf88243ec72e2c3f5126b3bb1bce21e6cecb
Alan Huynh <alan.hanhuynh@gmail.com>
2020-12-22 14:53:55 -0700

    commit test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,11 +4,25 @@ module.exports = function (app) {
 
     app.get("/api/playerName", async function (req, res) {
         try {
-            res.json(await db.playerName.findAll());
+            res.json(await db.playerName.findAll(
+                // `INSERT INTO playerName(
+                //     classType, name, health, attack, defense, speed, experience);`
+
+            ));
         } catch (err) {
             res.status(500).end();
         }
     });
+
+    app.post("/api/playerName", async function (req, res) {
+        try {
+          await db.playerName.create(req.body);
+          res.json({ msg: "Added Player!" });
+        } catch (err) {
+          res.status(500).end();
+        }
+      });
+
 
     // app.get("/api/playerName/:player", async function (req, res) {
     //     try {
